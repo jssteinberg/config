cd57015c97eb4df4b8272abc7fd77e5f4c7a9961
Steinberg <steinberg@hey.com>
2021-04-30 10:00:51 +0200

    For init, adjust comments and defaults

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,22 @@
--- global options
-vim.o.ignorecase = true
-vim.o.mouse = 'a'
+-- Global options
+vim.o.mouse = 'a' -- mouse can be used for all modes
 vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
-vim.o.title = true
-vim.o.gdefault = true -- default substitute to global
 vim.o.hidden = true -- allow hiding unsaved files
+-- searching
+vim.o.smartcase = true -- ignore case when lowercase
+vim.o.gdefault = true -- default substitute to global
 
--- window-local options
+-- Window-local options
 vim.wo.relativenumber = true
 vim.wo.list = true
 vim.wo.breakindent = true
 
--- buffer-local options
+-- Buffer-local options
 vim.bo.swapfile = false
 vim.bo.synmaxcol = 200
 vim.bo.copyindent = true
 
-require('maps')
-require('paq')
--- require('packer')
+-- Require
+require('maps') -- key mappings
+require('paq') -- plugins via paq
+-- require('packer') -- plugins via packer (does not work)
