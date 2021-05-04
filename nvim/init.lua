17ff0d60017801ea4257174b3a8e06ba73fdbb81
Steinberg <steinberg@hey.com>
2021-05-04 21:36:11 +0200

    Simplify plugins; replace with mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@
 vim.o.mouse = 'a' -- mouse can be used for all modes
 vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
 vim.o.hidden = true -- allow hiding unsaved files
-vim.o.sessionoptions = 'blank,curdir,folds,help,tabpages,winsize'
+-- vim.o.sessionoptions = 'blank,curdir,folds,help,tabpages,winsize'
 vim.o.ignorecase = true -- generally ignore case
 vim.o.listchars = vim.o.listchars .. ',tab:│ '
 -- searching
@@ -27,33 +27,59 @@ vim.bo.shiftwidth = 3 -- number of spaces for autoindent
 
 -- Key mappings
 
--- leader: space
+-- leader
+-- space
 vim.api.nvim_set_keymap('n', '<space>', '', {})
 vim.g.mapleader = ' ' -- 'vim.g' sets global variables
-
--- esc: qq
-vim.api.nvim_set_keymap('i', 'qq', '<esc>', {noremap = true})
-vim.api.nvim_set_keymap('x', 'qq', '<esc>', {noremap = true})
-vim.api.nvim_set_keymap('t', 'qq', '<c-\\><c-n>', {noremap = true}) -- esc to normal mode in :terminal
-
--- previous buffer: leader bb
-vim.api.nvim_set_keymap('n', '<leader>bb', '<c-^>', {noremap = true})
--- previous buffer (and esc terminal): leader bb
-vim.api.nvim_set_keymap('t', '<leader>bb', '<c-\\><c-n><c-^>', {noremap = true})
 
 -- open terminal in insert
 vim.api.nvim_set_keymap('n', '<leader>ot', ':terminal<cr>i', {noremap = true})
 
--- jump to definition: leader l
+-- jump to definition
 vim.api.nvim_set_keymap('n', '<leader>l', '<c-]>', {noremap = true})
 
+-- search
+-- visual star
+vim.api.nvim_set_keymap('x', '*', 'y/<c-r><cr>', {noremap = true})
+-- nohlsearch
+vim.api.nvim_set_keymap('n', '<cr>', ':nohlsearch<cr>', {noremap = true})
+
+-- esc
+-- qq in insert
+vim.api.nvim_set_keymap('i', 'qq', '<esc>', {noremap = true})
+-- qq in visual (x)
+vim.api.nvim_set_keymap('x', 'qq', '<esc>', {noremap = true})
+ -- qq in terminal
+vim.api.nvim_set_keymap('t', 'qq', '<c-\\><c-n>', {noremap = true})
+
+-- buffers
+-- list buffers, load by number
+vim.api.nvim_set_keymap('n', '<leader>bl', ':ls<cr>:b', {noremap = true})
+-- previous buffer
+vim.api.nvim_set_keymap('n', '<leader>bb', '<c-^>', {noremap = true})
+vim.api.nvim_set_keymap('t', '<leader>bb', '<c-\\><c-n><c-^>', {noremap = true})
+-- prev/next buffer
+vim.api.nvim_set_keymap('n', '<tab>', ':bn<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader><tab>', ':bp<cr>', {noremap = true})
+
+-- files
+-- wild find
+vim.api.nvim_set_keymap('n', '<leader>ff', ':find **/', {noremap = true})
+
+-- completion
+-- tab for buffer completion
+vim.api.nvim_set_keymap('i', '<tab>', '<c-n>', {noremap = true})
+vim.api.nvim_set_keymap('i', '<s-tab>', '<c-p>', {noremap = true})
+
 -- git
--- git commit current
+-- status
+vim.api.nvim_set_keymap('n', '<leader>gs', ':exe "!git status " . shellescape(getcwd())<cr>', {noremap = true})
+-- commit current
 vim.api.nvim_set_keymap('n', '<leader>gcc', ':!git commit "%" -m "', {noremap = true})
--- git commit all
+-- commit all
 vim.api.nvim_set_keymap('n', '<leader>gca', ':!git commit -am "', {noremap = true})
 
 -- Require
--- require('statusline')
+require('statusline')
 require('netrw')
 require('paq') -- plugins via paq
--- nvim/lua/paq.lua
+++ nvim/lua/paq.lua
@@ -3,33 +3,14 @@ vim.cmd 'packadd paq-nvim'         -- Load package
 local paq = require 'paq-nvim'.paq -- Import module and bind `paq` function
 paq {'savq/paq-nvim', opt=true}    -- Let Paq manage itself
 
-paq 'editorconfig/editorconfig-vim'  -- auto respect editorconfig files
-paq 'blackCauldron7/surround.nvim'   -- surround stuff with stuff (org. tpope/vim-surround)
-paq 'bronson/vim-visual-star-search' -- search visual with *
-paq 'b3nj5m1n/kommentary'            -- gcc, gc in visual mode, to (un)comment. Lua
-
--- Statusline
-paq 'hoob3rt/lualine.nvim' -- status line
-require('lualine').setup{
-	options = {
-		theme = 'tokyonight',
-		section_separators = {'',''},
-		component_separators = {'',''},
-		icons_enabled = false,
-	},
-	extensions = {'fzf'},
-	sections = {
-		lualine_a = {},
-		lualine_b = {'diff',},
-		lualine_c = {'branch','filename'},
-	},
-}
+paq 'editorconfig/editorconfig-vim' -- auto respect editorconfig files
+paq 'blackCauldron7/surround.nvim'  -- surround stuff with stuff (org. tpope/vim-surround)
+paq 'b3nj5m1n/kommentary'           -- gcc, gc in visual mode, to (un)comment. Lua
 
 -- Colorscheme(s)
 paq {'folke/tokyonight.nvim'}
 vim.cmd[[colorscheme tokyonight]]
 vim.g.tokyonight_style = "night"
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
 -- Align text
 paq 'junegunn/vim-easy-align'
@@ -37,11 +18,11 @@ vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {})
 vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', {})
 
 -- Fuzzy file search (fuzzy search more)
-paq {'junegunn/fzf', run='fzf#install()'}
-paq 'junegunn/fzf.vim'
-vim.api.nvim_set_keymap('n', '<leader>ff', ':FZF<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader><tab>', ':Buffers<cr>', {noremap = true})
-vim.api.nvim_set_keymap('x', '<leader>aa', 'y:Ag <c-r>"', {noremap = true})
+-- paq {'junegunn/fzf', run='fzf#install()'}
+-- paq 'junegunn/fzf.vim'
+-- vim.api.nvim_set_keymap('n', '<leader>ff', ':FZF<cr>', {noremap = true})
+-- vim.api.nvim_set_keymap('n', '<leader><tab>', ':Buffers<cr>', {noremap = true})
+-- vim.api.nvim_set_keymap('x', '<leader>aa', 'y:Ag <c-r>"', {noremap = true})
 
 -- Treesitter
 paq {'nvim-treesitter/nvim-treesitter'}
@@ -67,65 +48,3 @@ npairs.setup({
 require('nvim-treesitter.configs').setup {
 	autopairs = {enable = true}
 }
-
--- Auto completion
--- https://github.com/hrsh7th/nvim-compe
-paq 'hrsh7th/nvim-compe'
-vim.o.completeopt = "menuone,noselect"
-require'compe'.setup {
-	enabled = true;
-	autocomplete = true;
-	debug = false;
-	min_length = 1;
-	preselect = 'enable';
-	throttle_time = 80;
-	source_timeout = 200;
-	incomplete_delay = 400;
-	max_abbr_width = 100;
-	max_kind_width = 100;
-	max_menu_width = 100;
-	documentation = true;
-
-	source = {
-		path = true;
-		buffer = true;
-	};
-}
-local t = function(str)
-	return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-local check_back_space = function()
-	local col = vim.fn.col('.') - 1
-	if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-		return true
-	else
-		return false
-	end
-end
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-	if vim.fn.pumvisible() == 1 then
-		return t "<C-n>"
-	elseif vim.fn.call("vsnip#available", {1}) == 1 then
-		return t "<Plug>(vsnip-expand-or-jump)"
-	elseif check_back_space() then
-		return t "<Tab>"
-	else
-		return vim.fn['compe#complete']()
-	end
-end
-_G.s_tab_complete = function()
-	if vim.fn.pumvisible() == 1 then
-		return t "<C-p>"
-	elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-		return t "<Plug>(vsnip-jump-prev)"
-	else
-		return t "<S-Tab>"
-	end
-end
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
