c4e65ed27ba61040cb4e8e9829021f0086d1a32f
Steinberg <steinberg@hey.com>
2021-05-06 22:33:39 +0200

    Change comment plugin; remove gitsigns mappings (was an error)

--- nvim/lua/paq.lua
+++ nvim/lua/paq.lua
@@ -1,41 +1,33 @@
 -- Plugins via Paq (https://github.com/savq/paq-nvim)
 vim.cmd 'packadd paq-nvim'         -- Load package
 local paq = require 'paq-nvim'.paq -- Import module and bind `paq` function
-paq {'savq/paq-nvim', opt=true}    -- Let Paq manage itself
+paq {'savq/paq-nvim',opt=true}    -- Let Paq manage itself
 
 paq 'nvim-lua/plenary.nvim' -- lua functions, used by: telescope, gitsigns...
-
 paq'editorconfig/editorconfig-vim'  -- auto respect editorconfig files
-paq{'b3nj5m1n/kommentary',opt=true} -- gcc, gc in visual mode, to (un)comment. Lua
-
--- Align text
-paq {'junegunn/vim-easy-align', opt=true}
-vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {})
-vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', {})
 
 -- Colorscheme(s)
 paq {'folke/tokyonight.nvim'}
 vim.cmd[[colorscheme tokyonight]]
 vim.g.tokyonight_style = "night"
 
--- Git
-paq 'lewis6991/gitsigns.nvim'
-require('gitsigns').setup{
-	keymaps = {
-		['n <leader>hn'] = { expr = true, "&diff ? '<leader>hn' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
-		['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-		['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-		['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-		['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-		['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-		['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-	}
-}
-vim.o.statusline = ' ↯%{get(b:,"gitsigns_head","")} %{get(b:,"gitsigns_status","")}:' .. vim.o.statusline
+-- Comments
+paq{'terrortylor/nvim-comment'} -- gcc, gc in visual mode, to (un)comment. Lua
+require('nvim_comment').setup({})
 
 -- Surround
-paq {'blackCauldron7/surround.nvim',opt=true}  -- surround stuff with stuff (org. tpope/vim-surround)
-require("surround").setup({})
+paq {'blackCauldron7/surround.nvim'}  -- surround stuff with stuff (org. tpope/vim-surround)
+require('surround').setup({})
+
+-- Align text
+paq {'junegunn/vim-easy-align'}
+vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {})
+vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', {})
+
+-- Git
+paq 'lewis6991/gitsigns.nvim'
+require('gitsigns').setup({})
+vim.o.statusline = ' ↯%{get(b:,"gitsigns_head","")} %{get(b:,"gitsigns_status","")}:' .. vim.o.statusline
 
 -- Fuzzy file search (fuzzy search more)
 paq 'nvim-lua/popup.nvim'
