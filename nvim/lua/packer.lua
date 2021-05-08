4c41ab4843ea643807370df6547c7f01528e511a
Steinberg <steinberg@hey.com>
2021-05-08 23:26:03 +0200

    Format

--- nvim/lua/packer.lua
+++ nvim/lua/packer.lua
@@ -9,22 +9,22 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  -- Plugins can have dependencies on other plugins
---   use {
---     'haorenW1025/completion-nvim',
---     opt = true,
---     requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
---   }
+	-- Plugins can have dependencies on other plugins
+	-- use {
+	-- 	'haorenW1025/completion-nvim',
+	-- 	opt = true,
+	-- 	requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
+	-- }
 
-  -- Post-install/update hook with neovim command
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	-- Post-install/update hook with neovim command
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
-  -- Use dependency and run lua function after load
-  use {
-    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-    config = function() require('gitsigns').setup() end
-  }
+	-- Use dependency and run lua function after load
+	use {
+		'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+		config = function() require('gitsigns').setup() end
+	}
 end)
