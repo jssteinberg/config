1f94bfd9c0fbe614da625e1a373c7c332512a9f3
Steinberg <steinberg@hey.com>
2021-05-09 23:27:46 +0200

    Add comments of plugins to add

--- nvim/lua/paq.lua
+++ nvim/lua/paq.lua
@@ -24,6 +24,13 @@ paq{'junegunn/vim-easy-align'}
 vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {})
 vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', {})
 
+-- Plug 'mbbill/undotree', { 'on': 'UndotreeToggle' }
+-- let g:undotree_SetFocusWhenToggle=1
+-- nn <silent> <leader>fu :UndotreeToggle<CR>
+
+-- Plug 'junegunn/gv.vim', { 'on': 'GV'}
+-- nn <leader>gl :GV<CR>
+
 -- Git
 paq 'lewis6991/gitsigns.nvim'
 require('gitsigns').setup({})
