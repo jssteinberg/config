5548876003623435c515df4579e723af0933ce0e
Steinberg <steinberg@hey.com>
2021-05-09 21:53:12 +0200

    Remove opt=true
    I dont get it; didnt work as excpected.

--- nvim/lua/paq.lua
+++ nvim/lua/paq.lua
@@ -20,7 +20,7 @@ paq{'tpope/vim-surround'} -- surround stuff with stuff (org. tpope/vim-surround)
 paq{'tpope/vim-repeat'}   -- surround stuff with stuff (org. tpope/vim-surround)
 
 -- Align text
-paq{'junegunn/vim-easy-align',opt=true}
+paq{'junegunn/vim-easy-align'}
 vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {})
 vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', {})
 
@@ -31,7 +31,7 @@ vim.o.statusline = ' ↯%{get(b:,"gitsigns_head","")} %{get(b:,"gitsigns_status"
 
 -- Fuzzy file search (fuzzy search more)
 paq{'nvim-lua/popup.nvim'}
-paq{'nvim-telescope/telescope.nvim',opt=true}
+paq{'nvim-telescope/telescope.nvim'}
 local actions = require('telescope.actions')
 require('telescope').setup{
 	defaults = {
