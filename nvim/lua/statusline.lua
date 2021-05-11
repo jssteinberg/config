88b574ce1f5b4502e410a11f5a86959708c222f3
Steinberg <steinberg@hey.com>
2021-05-11 13:27:20 +0200

    Update statusline separators to en and em dash

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,2 +1,2 @@
-local sep = '--'
-return sep .. '-`:b%n` %m%f %y[%{&fileencoding}]%r%w ' .. sep .. ' (%l/%L):%c%<-' .. sep
+local sep = '–'
+return '—`:b%n` %m%f %y[%{&fileencoding}]%r%w ' .. sep .. ' (%l/%L):%c%<—'
