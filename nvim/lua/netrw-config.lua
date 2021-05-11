a596010bca1dcefbdbbac36f3dfa92f5ffec6404
Steinberg <steinberg@hey.com>
2021-05-11 21:46:00 +0200

    Added comments to netrw config

--- nvim/lua/netrw-config.lua
+++ nvim/lua/netrw-config.lua
@@ -1,2 +1,4 @@
-vim.api.nvim_set_keymap('n', '<leader>fb', ':e %:p:h<cr>', {noremap = true})
+-- No help banner
 vim.api.nvim_set_var('netrw_banner', 0)
+-- Open current buffer's folder
+vim.api.nvim_set_keymap('n', '<leader>fb', ':e %:p:h<cr>', {noremap = true})
