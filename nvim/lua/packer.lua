17c13244ac56293b6a7d970e18fdba2696b49e55
Steinberg <steinberg@hey.com>
2021-05-12 20:54:06 +0200

    More packer testing WIP

--- nvim/lua/packer.lua
+++ nvim/lua/packer.lua
@@ -1,30 +1,17 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+	fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+	execute 'packadd packer.nvim'
 end
 
-return require('packer').startup(function(use)
+require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
-	-- Plugins can have dependencies on other plugins
-	-- use {
-	-- 	'haorenW1025/completion-nvim',
-	-- 	opt = true,
-	-- 	requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
-	-- }
-
 	-- Post-install/update hook with neovim command
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-
-	-- Use dependency and run lua function after load
-	use {
-		'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-		config = function() require('gitsigns').setup() end
-	}
 end)
