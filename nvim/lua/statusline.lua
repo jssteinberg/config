12c0fa9de07783a3c3c9701d9969432f5bb69bcd
Steinberg <steinberg@hey.com>
2021-05-12 15:14:29 +0200

    Add seperator to end of statusline also

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,2 +1,2 @@
 local sep = '--'
-return sep .. ' `:b%n` %f%m %#LineNr# %y[%{&fileencoding}]%r%w ' .. sep .. ' (%l/%L):%c %<%='
+return sep .. ' `:b%n` %f%m %#LineNr# %y[%{&fileencoding}]%r%w ' .. sep .. ' (%l/%L):%c ' .. sep .. '%<%='
