d8c2c15ec54e3ef379390e886d783e74034c9b1a
Steinberg <steinberg@hey.com>
2021-05-12 10:04:33 +0200

    Update statusline bg (hi group to LineNr)

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,2 +1,2 @@
-local sep = '–'
-return '—`:b%n` %m%f %y[%{&fileencoding}]%r%w ' .. sep .. ' (%l/%L):%c%<%#StatusEnd#—%='
+local sep = '--'
+return ' `:b%n` %m%f %#LineNr# %y[%{&fileencoding}]%r%w ' .. sep .. ' (%l/%L):%c %<%='
