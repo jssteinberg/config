21eb3e2f44abc16aec14e40c59d9aa8041e80459
Steinberg <steinberg@hey.com>
2021-05-12 19:11:41 +0200

    Change statusline hilight end group to LineNr

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,2 +1,2 @@
 local sep = '--'
-return sep .. ' `:b%n` %f%m %#Comment# %y[%{&fileencoding}]%r%w ' .. sep .. ' (%l/%L):%c ' .. sep .. '%<%='
+return sep .. ' `:b%n` %f%m %#LineNr# %y[%{&fileencoding}]%r%w ' .. sep .. ' (%l/%L):%c ' .. sep .. '%<%='
