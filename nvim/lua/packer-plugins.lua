06a2edfd1405cdc52447f723ed4f7b55f5c0f459
Steinberg <steinberg@hey.com>
2021-05-15 13:00:05 +0200

    Update comments

--- nvim/lua/packer-plugins.lua
+++ nvim/lua/packer-plugins.lua
@@ -1,49 +1,84 @@
 require('packer').startup(function(use)
-	-- Packer can manage itself
+	-- Let Packer manage itself
 	use 'wbthomason/packer.nvim'
 
+	-- No config plugins
 	use{'editorconfig/editorconfig-vim'} -- auto respect editorconfig files
-	use{'mmozuras/vim-cursor'}           -- when open buffer, go to previous cursor position
 	use{'subnut/visualstar.vim'}         -- visual star `*` search, or `#` backwards
 
-	-- Post-install/update hook with neovim command
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	-- Load on BufEnter
+	use{'tpope/vim-surround', event = 'BufEnter'}  -- surround stuff with stuff (org. tpope/vim-surround)
+	use{'tpope/vim-repeat', event = 'BufEnter'}    -- repeat surround and more
+	use{'mmozuras/vim-cursor', event = 'BufEnter'} -- when open buffer, go to previous cursor position
 
 	-- Whickkey to map keymappings
 	use{'folke/which-key.nvim'}
 	local wk = require("which-key")
-	wk.setup {}
+	wk.setup({})
 
-	-- Surround & Repeat
-	use{'tpope/vim-surround'} -- surround stuff with stuff (org. tpope/vim-surround)
-	use{'tpope/vim-repeat'}   -- repeat surround and more
+	-- Treesitter for syntax
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		event = 'BufEnter',
+		run = ':TSUpdate',
+		config = function()
+			require('nvim-treesitter.configs').setup{
+				ensure_installed = 'maintained',
+				highlight = {enable = true},
+			}
+		end
+	}
 
 	-- Colorscheme(s)
 	use{'folke/tokyonight.nvim'}
-	-- vim.g.tokyonight_colors = { comment = "green" }
 	vim.cmd[[colorscheme tokyonight]]
-	vim.g.tokyonight_style = 'night'
+	vim.api.nvim_set_var('tokyonight_style', 'night')
 
 	-- Comments
-	use{'terrortylor/nvim-comment'} -- gcc, gc in visual mode, to (un)comment. Lua
-	require('nvim_comment').setup({})
+	-- gcc, gc in visual mode, to (un)comment. Lua
+	use{
+		'terrortylor/nvim-comment',
+		keys = {{'n','gcc'}, {'x','gc'},},
+		config = function()
+			require('nvim_comment').setup({})
+		end
+	}
 
 	-- Open URI /search in browser
-	use{'tyru/open-browser.vim'}
-	vim.api.nvim_set_var('netrw_nogx', 1) -- disable netrw's gx mapping.
-	vim.api.nvim_set_var('openbrowser_default_search', 'duckduckgo')
+	use{
+		'tyru/open-browser.vim',
+		-- cmd = {'<Plug>(openbrowser-smart-search)'}, -- gives invalid command name on PackerCompile...
+		-- keys = { {'n','gx'}, {'v','gx'} },
+		event = 'BufEnter',
+		config = function()
+			vim.api.nvim_set_var('netrw_nogx', 1) -- disable netrw's gx mapping.
+			vim.api.nvim_set_var('openbrowser_default_search', 'duckduckgo')
+		end
+	}
 	vim.api.nvim_set_keymap('n', 'gx', '<Plug>(openbrowser-smart-search)', {})
 	vim.api.nvim_set_keymap('v', 'gx', '<Plug>(openbrowser-smart-search)', {})
 
 	-- Align text
-	use{'junegunn/vim-easy-align', cmd = {'<Plug>(EasyAlign)'}}
+	use{
+		'junegunn/vim-easy-align',
+		-- cmd = {'<Plug>(EasyAlign)'} -- gives invalid command name on PackerCompile...
+		-- keys = {{'n','ga'}, {'x', 'ga'},}
+		event = 'BufEnter',
+	}
 	vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {})
 	vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', {})
 
 	-- Git
-	use{'tpope/vim-fugitive', cmd = {'G', 'Git'}}
-	-- log
-	use{'junegunn/gv.vim', cmd = {'GV'}} -- requires vim-fugitive
+	-- fugitive
+	use{'tpope/vim-fugitive'}
+	-- git log
+	use{
+		'junegunn/gv.vim',
+		cmd = {'GV'},
+		requires = {
+			{'tpope/vim-fugitive'},
+		}
+	}
 	vim.api.nvim_set_keymap('n', '<leader>gl', ':GV<cr>', {noremap = true})
 	-- diff view
 	use{
@@ -60,72 +95,118 @@ require('packer').startup(function(use)
 	}
 	vim.api.nvim_set_keymap('n', '<leader>gd', ':DiffviewOpen<cr>', {noremap = true})
 	-- git blame
-	use{'f-person/git-blame.nvim'}
+	use{'f-person/git-blame.nvim', event = 'BufEnter'}
 
-	-- Auto completion
-	use{'hrsh7th/nvim-compe'}
-	vim.o.completeopt = "menuone,noselect"
-	require'compe'.setup {
-		enabled = true;
-		autocomplete = true;
-		debug = false;
-		min_length = 1;
-		preselect = 'enable';
-		throttle_time = 80;
-		source_timeout = 200;
-		incomplete_delay = 400;
-		max_abbr_width = 100;
-		max_kind_width = 100;
-		max_menu_width = 100;
-		documentation = true;
+ 	-- Auto completion
+	use{
+		'hrsh7th/nvim-compe',
+		event = 'InsertEnter',
+		config = function()
+			vim.o.completeopt = "menuone,noselect"
+			require'compe'.setup {
+				enabled = true;
+				autocomplete = true;
+				debug = false;
+				min_length = 1;
+				preselect = 'enable';
+				throttle_time = 80;
+				source_timeout = 200;
+				incomplete_delay = 400;
+				max_abbr_width = 100;
+				max_kind_width = 100;
+				max_menu_width = 100;
+				documentation = true;
 
-		source = {
-			path = true;
-			buffer = true;
-			-- calc = true;
-			-- nvim_lsp = true;
-			nvim_lua = true;
-			-- vsnip = true;
-			-- ultisnips = true;
-		};
+				source = {
+					path = true;
+					buffer = true;
+					-- calc = true;
+					-- nvim_lsp = true;
+					-- nvim_lua = true;
+					-- vsnip = true;
+					-- ultisnips = true;
+				};
+			}
+
+			local t = function(str)
+				return vim.api.nvim_replace_termcodes(str, true, true, true)
+			end
+			local check_back_space = function()
+				local col = vim.fn.col('.') - 1
+				if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+					return true
+				else
+					return false
+				end
+			end
+
+			-- Use (s-)tab to:
+			--- move to prev/next item in completion menuone
+			--- jump to prev/next snippet's placeholder
+			_G.tab_complete = function()
+				if vim.fn.pumvisible() == 1 then
+					return t "<C-n>"
+				elseif vim.fn.call("vsnip#available", {1}) == 1 then
+					return t "<Plug>(vsnip-expand-or-jump)"
+				elseif check_back_space() then
+					return t "<Tab>"
+				else
+					return vim.fn['compe#complete']()
+				end
+			end
+			_G.s_tab_complete = function()
+				if vim.fn.pumvisible() == 1 then
+					return t "<C-p>"
+				elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+					return t "<Plug>(vsnip-jump-prev)"
+				else
+					return t "<S-Tab>"
+				end
+			end
+
+			-- bind (s-)tab
+			vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+			vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+			vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+			vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+		end
 	}
-	local t = function(str)
-		return vim.api.nvim_replace_termcodes(str, true, true, true)
-	end
-	local check_back_space = function()
-		local col = vim.fn.col('.') - 1
-		if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-			return true
-		else
-			return false
+
+	-- Telescope for fuzzy fast finding
+	use{
+		'nvim-telescope/telescope.nvim',
+		cmd = 'Telescope',
+		requires = {
+			{'nvim-lua/popup.nvim'},
+			{'nvim-lua/plenary.nvim'}
+		},
+		config = function()
+			local actions = require('telescope.actions')
+			require('telescope').setup({
+				defaults = {
+					mappings = {
+						i = {
+							['qq'] = actions.close
+						}
+					}
+				}
+			})
 		end
-	end
-	-- Use (s-)tab to:
-	--- move to prev/next item in completion menuone
-	--- jump to prev/next snippet's placeholder
-	_G.tab_complete = function()
-		if vim.fn.pumvisible() == 1 then
-			return t "<C-n>"
-		elseif vim.fn.call("vsnip#available", {1}) == 1 then
-			return t "<Plug>(vsnip-expand-or-jump)"
-		elseif check_back_space() then
-			return t "<Tab>"
-		else
-			return vim.fn['compe#complete']()
-		end
-	end
-	_G.s_tab_complete = function()
-		if vim.fn.pumvisible() == 1 then
-			return t "<C-p>"
-		elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-			return t "<Plug>(vsnip-jump-prev)"
-		else
-			return t "<S-Tab>"
-		end
-	end
-	vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-	vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-	vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-	vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+	}
+
+	vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {})
+	vim.api.nvim_set_keymap('n', '<leader>fr', '<cmd>Telescope oldfiles<cr>',  {})
+	vim.api.nvim_set_keymap('n', '<leader><tab>', '<cmd>Telescope buffers<cr>',    {}) -- change tab
+	local telescope_mappings = {
+		["<leader>ff"] = {
+			"<cmd>Telescope find_files<cr>",
+			"Find File",
+		},
+		["<leader>fr"] = {
+			"<cmd>Telescope oldfiles<cr>",
+			"Open Recent File"
+		}
+	}
+	wk.register(telescope_mappings)
 
 end)
