e454320c1d3148d9e3c55ebf11550ff27a6fbd68
Steinberg <steinberg@hey.com>
2021-10-28 22:40:56 +0200

    Commented out nvim compe with pears.nvim intergration

--- nvim/lua/packages/init.lua
+++ nvim/lua/packages/init.lua
@@ -92,11 +92,11 @@ require('packer').startup(function(use)
 		require'pears'.setup(function(conf)
 			conf.preset 'tag_matching'
 			conf.on_enter(function(pears_handle)
-				if vim.fn.pumvisible() == 1 and vim.fn.complete_info().selected ~= -1 then
-					return vim.fn['compe#confirm']('<CR>')
-				else
+				-- if vim.fn.pumvisible() == 1 and vim.fn.complete_info().selected ~= -1 then
+				-- 	return vim.fn['compe#confirm']('<CR>')
+				-- else
 					pears_handle()
-				end
+				-- end
 			end)
 		end)
 	end}
