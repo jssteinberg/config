d0d8dd9453967f250eeb30acf08593e5f6d566a6
Steinberg <steinberg@hey.com>
2021-11-27 12:11:26 +0100

    Nvim, change back to tokyonight

--- nvim/lua/packages/init.lua
+++ nvim/lua/packages/init.lua
@@ -18,7 +18,8 @@ require('packer').startup(function(use)
 	use{'folke/which-key.nvim'} require'packages.which-key'.config() -- Keymappings popup
 	use{'projekt0n/github-nvim-theme'}
 	use{'folke/tokyonight.nvim'}
-	require'packages.colors'.github_theme_config()
+	require'packages.colors'.tokyonight_config()
+	-- require'packages.colors'.github_theme_config()
 	use{'svermeulen/vim-yoink'} require'packages.yoink'.init() -- Cycle yank history on paste
 	use{'nvim-lualine/lualine.nvim'} require('packages.lualine').config()
 	-- use { 'glepnir/galaxyline.nvim', branch = 'main', config = function() require'packages.galaxyline'.config() end}
