c9fc4803117d5d6aa5d14192d489751510941073
Steinberg <steinberg@hey.com>
2023-03-17 23:58:58 +0100

    feat(nvim): fix oil mappings

--- nvim/lua/packages/oil.lua
+++ nvim/lua/packages/oil.lua
@@ -2,12 +2,18 @@ return function()
 	vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
 
 	require "oil".setup({
+		use_default_keymaps = false,
 		keymaps = {
+			["g?"] = "actions.show_help",
+			["<CR>"] = "actions.select",
 			["<c-j>"] = "actions.select",
 			["<c-p>"] = "actions.select_vsplit",
 			["<c-s>"] = "actions.select_split",
 			["<c-t>"] = "actions.select_tab",
 			["<c-k>"] = "actions.preview",
+			["<C-l>"] = "actions.refresh",
+			["-"] = "actions.parent",
+			["_"] = "actions.open_cwd",
 			["cd"] = "actions.tcd",
 			["gh"] = "actions.toggle_hidden",
 			["."] = "open_cmdline",
