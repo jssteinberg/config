d97cf57dbb8efb8784f55f2ff7fdbbf6541a9ed4
Steinberg <steinberg@hey.com>
2023-12-08 12:00:23 +0100

    refactor(wezterm): font-size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,7 +13,7 @@ end
 -- config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = .925
-config.font_size = 13
+config.font_size = 13.5
 config.color_scheme = "Ibm 3270 (High Contrast) (Gogh)"
 config.use_dead_keys = false
 config.window_decorations = "RESIZE"
