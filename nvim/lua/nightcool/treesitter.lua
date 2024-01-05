e12518e0ea1d132b1e8cffb44eabcecc6c31990d
jssteinberg <steinberg@hey.com>
2024-01-05 21:06:11 +0100

    refactor(nvim): hi return bold

--- nvim/lua/nightcool/treesitter.lua
+++ nvim/lua/nightcool/treesitter.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_set_hl(0, "@constructor", { link = "@tag" }) -- keep like tag since often custom HTML element from ES import
 vim.api.nvim_set_hl(0, "@function", { link = "@variable" })
 vim.api.nvim_set_hl(0, "@keyword", { ctermfg = 12, fg = "#9fb4d8" })
-vim.api.nvim_set_hl(0, "@keyword.return", { ctermfg = 3, fg = "#bc985c" })
+vim.api.nvim_set_hl(0, "@keyword.return", { ctermfg = 3, fg = "#bc985c", bold = true })
 vim.api.nvim_set_hl(0, "@label", { link = "Contant" })
 vim.api.nvim_set_hl(0, "@property", { link = "String" }) -- CSS classes are `@property`. Keep same as String since classes in HTML are strings
 vim.api.nvim_set_hl(0, "@property.vim", { link = "Normal" })
