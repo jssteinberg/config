225bda327dcf367ffd89781354f3f7a2f816f9d5
Steinberg <steinberg@hey.com>
2024-02-18 06:47:20 +0100

    refactro(nvim): colo, test treesitter keyword reset

--- nvim/lua/nightcool/treesitter.lua
+++ nvim/lua/nightcool/treesitter.lua
@@ -1,7 +1,9 @@
 vim.api.nvim_set_hl(0, "@constructor", { link = "@tag" }) -- keep like tag since often custom HTML element from ES import
 vim.api.nvim_set_hl(0, "@function", { link = "@variable" })
 vim.api.nvim_set_hl(0, "@lsp.type.function", { link = "@variable" })
-vim.api.nvim_set_hl(0, "@keyword", { ctermfg = 12, fg = "#9fb4d8" })
+-- vim.api.nvim_set_hl(0, "@keyword", { ctermfg = 12, fg = "#9fb4d8" })
+-- vim.api.nvim_set_hl(0, "@keyword.javascript", {})
+vim.api.nvim_set_hl(0, "@keyword", {})
 vim.api.nvim_set_hl(0, "@keyword.return", { ctermfg = 3, fg = "#bc985c", bold = true })
 vim.api.nvim_set_hl(0, "@label", { link = "Contant" })
 vim.api.nvim_set_hl(0, "@property", { link = "String" }) -- CSS classes are `@property`. Keep same as String since classes in HTML are strings
