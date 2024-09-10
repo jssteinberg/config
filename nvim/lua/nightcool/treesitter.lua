bbb5be0c4416a977601e0b3c1791bf97fa0f2bac
jssteinberg <johan@Johans-MacBook-Pro.local>
2024-09-10 15:23:05 +0200

    refactor(nightcool): uniform keyword likes

--- nvim/lua/nightcool/treesitter.lua
+++ nvim/lua/nightcool/treesitter.lua
@@ -1,3 +1,4 @@
+vim.api.nvim_set_hl(0, "@attribute", { link = "Keyword" })
 vim.api.nvim_set_hl(0, "@variable", { ctermfg = 15, fg = "#e9f5e6" })
 vim.api.nvim_set_hl(0, "@variable.object", { link = "Constant" })
 vim.api.nvim_set_hl(0, "@constructor", { link = "@tag" }) -- keep like tag since often custom HTML element from ES import
@@ -8,7 +9,7 @@ vim.api.nvim_set_hl(0, "@lsp.type.function", { link = "@variable" })
 -- vim.api.nvim_set_hl(0, "@keyword", {})
 vim.api.nvim_set_hl(0, "@keyword.conditional", { fg = "#9fb4d8" })
 vim.api.nvim_set_hl(0, "@keyword.return", { ctermfg = 3, fg = "#bc985c", bold = true })
-vim.api.nvim_set_hl(0, "@keyword.import", { ctermfg = 3, fg = "#5cb1bc" })
+vim.api.nvim_set_hl(0, "@keyword.import", { ctermfg = 6, fg = "#5cb1bc" })
 vim.api.nvim_set_hl(0, "@label", { fg = "#9fb4d8" })
 vim.api.nvim_set_hl(0, "@none.svelte", { link = "@label" })
 vim.api.nvim_set_hl(0, "@property", { link = "String" }) -- CSS classes are `@property`. Keep same as String since classes in HTML are strings
