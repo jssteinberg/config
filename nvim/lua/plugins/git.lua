5c6b6ec2e5ff925b00766a0cfc70266c4a7a46e4
jssteinberg <steinberg@hey.com>
2026-04-14 09:13:37 +0200

    chore(nvim): disable neogit

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,12 +1,12 @@
 return {
-	{
-		"NeogitOrg/neogit",
-		cmd = "Neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"sindrets/diffview.nvim", -- optional - Diff integration
-		},
-	},
+	-- {
+	-- 	"NeogitOrg/neogit",
+	-- 	cmd = "Neogit",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim", -- required
+	-- 		"sindrets/diffview.nvim", -- optional - Diff integration
+	-- 	},
+	-- },
 	-- Git wrapper
 	{ "tpope/vim-fugitive", cmd = { "G", "Git", "Gdiffsplit", "Gvdiffsplit", "Flog", "Flogsplit" } },
 	-- git log
